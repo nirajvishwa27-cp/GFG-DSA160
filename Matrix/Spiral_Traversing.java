1110ae7b3d12d2056937dd8115b50c4c2771061d
nirajvishwa27 <nirajvishwa27@gmail.com>
2024-12-20 19:57:06 +0530

    Pushed

---
+++ Matrix/Spiral_Traversing.java
@@ -0,0 +1,64 @@
+You are given a rectangular matrix mat[][] of size n x m, and your task is to return an array while traversing the matrix in spiral form.
+
+Examples:
+
+Input: mat[][] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+Output: [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+Explanation: 
+
+Input: mat[][] = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]]
+Output: [1, 2, 3, 4, 5, 6, 12, 18, 17, 16, 15, 14, 13, 7, 8, 9, 10, 11]
+Explanation: Applying same technique as shown above.
+Input: mat[][] = [[32, 44, 27, 23], [54, 28, 50, 62]]
+Output: [32, 44, 27, 23, 62, 50, 28, 54]
+Explanation: Applying same technique as shown above, output will be [32, 44, 27, 23, 62, 50, 28, 54].
+
+CODE:
+
+
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int mat[][]) {
+        // code here
+       ArrayList<Integer> ans = new ArrayList<>();
+        
+        int n = mat.length; // no. of rows
+        int m = mat[0].length; // no. of columns
+        
+        // Initialize the pointers required for traversal.
+        int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+        // Loop until all elements are not traversed.
+        while (top <= bottom && left <= right) {
+
+            // For moving left to right
+            for (int i = left; i <= right; i++)
+                ans.add(mat[top][i]);
+
+            top++;
+
+            // For moving top to bottom.
+            for (int i = top; i <= bottom; i++)
+                ans.add(mat[i][right]);
+
+            right--;
+
+            // For moving right to left.
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    ans.add(mat[bottom][i]);
+
+                bottom--;
+            }
+
+            // For moving bottom to top.
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.add(mat[i][left]);
+
+                left++;
+            }
+        }
+        return ans;
+    }
+}
